dd887a6da3c4e7a96a69a31936dd5bb669ecc4fe
Atif Afzal <atif5801@gmail.com>
2019-08-16 00:06:49 +0530

    Move header routes to routes index

--- #/routes/index.tsx
+++ #/routes/index.tsx
@@ -1,13 +1,17 @@
 import Header from '#/components/header';
 import '#/effects/protected-routes';
-import { Route, RouteSwitch } from '#/router';
+import { location, Route, RouteSwitch } from '#/router';
+import { Show } from 'solid-js/dom';
 import HomeRoute from './home';
 import LoginRoute from './login';
 
 function Routes() {
   return (
     <div>
-      <Header />
+      <Show when={(void 0, ['/', '/login'].includes(location()))}>
+        <Header />
+      </Show>
+
       <RouteSwitch>
         <Route path="/">
           <HomeRoute />
