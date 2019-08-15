89884b2e39f9cdd0b85e715b71d3d013d02ec77a
Atif Afzal <atif5801@gmail.com>
2019-08-15 19:58:28 +0530

    Use style for transform

--- #/modules/header/pipelines.ts
+++ #/modules/header/pipelines.ts
@@ -1,22 +1,17 @@
 import { history } from '#/router';
-import { css } from 'emotion';
-
-const translateCss = css`
-  transform: translate(-550px, -160px) scale(1.5);
-`;
 
 export function appHeaderPipeline(element: HTMLElement) {
   const unlisten = history.listen(updateStyles);
 
-  function updateStyles({ pathname }: { pathname: string }) {
+  function updateStyles(location: { pathname: string }) {
     if (!element) {
       unlisten();
     }
 
-    if (pathname === '/') {
-      element.classList.add(translateCss);
+    if (location.pathname === '/') {
+      element.style.transform = 'translate(-550px, -160px) scale(1.5)';
     } else {
-      element.classList.remove(translateCss);
+      element.style.transform = 'none';
     }
   }
 
