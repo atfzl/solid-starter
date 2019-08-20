813aa8a3407f23d57e32ba54dac99d5b8f14dd21
Atif Afzal <atif5801@gmail.com>
2019-08-21 01:26:18 +0530

    remove dead code

--- #/modules/header/pipeline.ts
+++ #/modules/header/pipeline.ts
@@ -5,10 +5,6 @@ export function appHeaderPipeline(element: HTMLElement) {
   createEffect(() => {
     const pathname = location();
 
-    if (!element) {
-      return;
-    }
-
     if (pathname === '/') {
       element.style.transform = 'translate(-565px, -160px) scale(0.7)';
     } else {
