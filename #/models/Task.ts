6a221b2efcbc938c3d7aca363ce8b4ee31a9157f
Atif Afzal <atif5801@gmail.com>
2019-09-14 23:59:13 +0530

    update

---
+++ #/models/Task.ts
@@ -0,0 +1,10 @@
+export interface TaskModel {
+  id: string;
+  text: string;
+  checklists: Array<{ text: string }>;
+  note?: string;
+  tags: Record<string, true>;
+  scheduledDate?: Date;
+  projectId?: string;
+  superTags: Record<string, true>;
+}
