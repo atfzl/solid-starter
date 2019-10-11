8ebe37e8230c1f697c9c630434ea70a751cdec88
Atif <atif5801@gmail.com>
2019-10-11 13:39:18 +0530

    Add webpack-bundle-analyzer

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,10 @@
 import * as ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 const isProd = process.env.NODE_ENV === 'production';
+const isAnalyze = !!process.env.ANALYZE;
 
 module.exports = {
   mode: !isProd ? 'development' : 'production',
@@ -31,7 +33,8 @@ module.exports = {
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
     new HtmlWebpackPlugin({ template: '#/index.html' }),
-  ],
+    isAnalyze && new BundleAnalyzerPlugin(),
+  ].filter(a => !!a),
 
   devServer: {
     stats: 'minimal',
