df9a68a1acf11fe5e304b7e217919b1a662ecc7c
Atif Afzal <atif5801@gmail.com>
2019-12-07 02:19:47 +0530

    Remove packages

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,13 +2,3 @@ declare module '@storybook/html' {
   const x: { storiesOf: any; configure: any };
   export = x;
 }
-
-declare module 'pouchdb-browser' {
-  const x: PouchDB.Static;
-  export default x;
-}
-
-declare module 'memorystream' {
-  const x: any;
-  export = x;
-}
