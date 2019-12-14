4b91db9e0361a784cb6b22ec9bb4a10e3b34a206
Atif Afzal <atif5801@gmail.com>
2019-12-14 21:03:35 +0530

    redirect to hash home

--- #/modules/routing/routing.pipelines.ts
+++ #/modules/routing/routing.pipelines.ts
@@ -1,6 +1,14 @@
+import { createEffect } from 'solid-js';
 import { routingActions } from './routing.actions';
+import { routingState } from './routing.state';
 
 export const routingPipelines = () => {
+  createEffect(() => {
+    if (!routingState.locationHash.startsWith('#')) {
+      window.location.hash = '#/';
+    }
+  });
+
   window.addEventListener('hashchange', () => {
     routingActions.setLocation(window.location.hash);
   });
